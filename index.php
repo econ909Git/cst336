9873066a49fd4090a2d6bedd9f8068deca633f1e
John Economides <jeconomides@csumb.edu>
2018-09-08 08:21:24 +0000

    added index.php file with path to Labs folder

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
-<html><body>
-<?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
-
-echo "<h2> John Economides' CST3336 Site </h2>";
-
-?>
-</body>
+<!DOCTYPE html>
+<html>
+    <nav>
+        <a href="Labs"> Labs</a>
+    
+    </nav>
 </html>
