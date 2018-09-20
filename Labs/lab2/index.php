5dfb064d8a553d7804bb7a5e74981956a3e07c74
John Economides <jeconomides@csumb.edu>
2018-09-20 20:46:13 +0000

    lab2 complete and ready to submit

--- Labs/lab2/index.php
+++ Labs/lab2/index.php
@@ -1,76 +1,27 @@
 <!DOCTYPE html>
+
+<?php
+    include 'inc/functions.php';
+?>
+
 <html>
     <head>
         <title> 777 Slot Machine </title>
+        <style>
+            @import url("css/styles.css");
+        </style>
     </head>
     <body>
         <!--<img src="img/cherry.png" alt="cherry" title="Cherry" width="70" /> -->
+    <div id="main" >  
         <?php
-        // $randomValue1 = rand(0,2);
-        // displaySymbol($randomValue1);
-        // $randomValue2 = rand(0,2);
-        // displaySymbol($randomValue2);
-        // $randomValue3 = rand(0,2);
-        // displaySymbol($randomValue3);
-        
-       for($i=1; $i<4; $i++){
-           ${"randomValue" . $i } = rand(0,2);
-           displaySymbol(${"randomValue" . $i} );
-       }
-        displayPoints($randomValue1, $randomValue2, $randomValue3);
-        
-        function displaySymbol($randomValue){
-                
-                switch($randomValue){
-                    case 0: $symbol = "grapes";
-                           break;
-                    case 1: $symbol = "orange";
-                           break;
-                    case 2: $symbol = "cherry";
-                           break;
-                }
-            
-            echo "<img src=\"img/$symbol.png\" alt='symbol' title='".ucfirst($symbol)."' width='70' />";
-            
-            //echo "random value= " . $randomValue;
-            
-            
-            // $randomValue = rand(0,2);
-            // if($randomValue == 0){
-            //     echo '<img src="img/seven.png" alt="seven" title="Seven" width="70" />';
-            // }
-            // else if($randomValue == 1){
-            //     echo '<img src="img/cherry.png" alt="cherry" title="Cherry" width="70" />';
-            // }
-            // else {
-            //     //echo '<img src="img/lemon.png" alt="lemon" title="Lemon" width="70" />';
-            //     echo "<img src=\"img/$symbolLemon.png\" alt='lemon' title='".ucfirst($symbolLemon)."' width='70' />";
-            // }
-            
-            
-        }
-        
-        function displayPoints($randomValue1, $randomValue2, $randomValue3){
-            
-            echo "<div id='output'>";
-            if($randomValue1 == $randomValue2 && $randomValue2 == $randomValue3) {
-                switch($randomValue1){
-                    case 0: $totalPoints == 1000;
-                            echo "<h1>JACKPOT!</h1>";
-                            break;
-                    case 1: $totalPoints == 500;
-                            break;
-                    case 2: $totalPoints == 250;
-                            break;
-                }
-                echo "<h2>You won $totalPoints points!</h2>";
-                
-            }
-            else{
-                echo"<h3> Try Agian! </h3>";
-            }
-            echo "</div>";
-        }
+            play();  
         ?>
+        
+        <form>
+            <input type="submit" value="Spin!"/>
+        </form>
+        
+    </div>    
     </body>
 </html>
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
     <nav>
-        <a href="Labs/lab1"> Lab1 </a>
-        <a href="Homework/HW1"> Homework 1 </a>
+        <a href="Labs/lab1"> Lab1  </a>
+        <a href="Labs/lab2"> Lab2  </a>
+        <a href="Homework/HW1"> Homework 1  </a>
     </nav>
 </html>
