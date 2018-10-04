329f5949634df196ea61d3605a173762ce577699
John Economides <jeconomides@csumb.edu>
2018-10-04 06:56:39 +0000

    added index for HW2

--- index.php
+++ index.php
@@ -4,5 +4,6 @@
         <a href="Labs/lab1"> Lab1  </a>
         <a href="Labs/lab2"> Lab2  </a>
         <a href="Homework/HW1"> Homework 1  </a>
+        <a href="Homework/HW2"> Homework 1  </a>
     </nav>
 </html>
