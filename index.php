544bd37a64588472ec64d980eafea077b94cd074
John Economides <jeconomides@csumb.edu>
2018-10-06 08:58:28 +0000

    added lab 4 index link

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <a href="Labs/lab1"> Lab1  </a>
         <a href="Labs/lab2"> Lab2  </a>
         <a href="Labs/lab3"> Lab3  </a>
+        <a href="Labs/lab4"> Lab4  </a>
         <a href="Homework/HW1"> Homework 1  </a>
         <a href="Homework/HW2"> Homework 2  </a>
     </nav>
