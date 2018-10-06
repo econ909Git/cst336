aad80f5dd8914c5665ddb76a97b9c34e205c6f59
John Economides <jeconomides@csumb.edu>
2018-10-06 06:55:05 +0000

    added silver jack index link

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
     <nav>
         <a href="Labs/lab1"> Lab1  </a>
         <a href="Labs/lab2"> Lab2  </a>
+        <a href="SilverJack/silverjack"> Lab3  </a>
         <a href="Homework/HW1"> Homework 1  </a>
-        <a href="Homework/HW2"> Homework 1  </a>
+        <a href="Homework/HW2"> Homework 2  </a>
     </nav>
 </html>
