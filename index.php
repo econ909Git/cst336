8c595608690ca13b36d5492ef2b9edb1788116ab
John Economides <jeconomides@csumb.edu>
2018-10-17 22:04:16 +0000

    made link to HW3

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
         <a href="Labs/lab4/Slider"> Lab4  </a>
         <a href="Homework/HW1"> Homework 1  </a>
         <a href="Homework/HW2"> Homework 2  </a>
-        <a href="Homework/HW3"> Homework 3  </a>
+        <br>
+        <a href="Homework/HW2"> Homework 3  </a>
     </nav>
 </html>
