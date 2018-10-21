ca0dfb409e3b4e5dbc5e17c8d4150cf905bb8b9f
John Economides <jeconomides@csumb.edu>
2018-10-21 09:29:49 +0000

    added correct link to lab 5

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <a href="Labs/lab2"> Lab2  </a><br>
         <a href="Labs/lab3"> Lab3  </a><br>
         <a href="Labs/lab4/Slider"> Lab4  </a><br>
-        <a href="Labs/lab5/Slider"> Lab5  </a><br>
+        <a href="Labs/lab5/scart"> Lab5  </a><br>
         <a href="Homework/HW1"> Homework 1  </a><br>
         <a href="Homework/HW2"> Homework 2  </a><br>
         <a href="Homework/HW3"> Homework 3  </a><br>
