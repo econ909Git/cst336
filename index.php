6c9be1218eebbf58a294f928d276b72c9fa2669d
John Economides <jeconomides@csumb.edu>
2018-10-21 09:27:12 +0000

    added lab 5 and link to it

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
     <nav>
-        <a href="Labs/lab1"> Lab1  </a>
-        <a href="Labs/lab2"> Lab2  </a>
-        <a href="Labs/lab3"> Lab3  </a>
-        <a href="Labs/lab4/Slider"> Lab4  </a>
-        <a href="Homework/HW1"> Homework 1  </a>
+        <a href="Labs/lab1"> Lab1  </a><br>
+        <a href="Labs/lab2"> Lab2  </a><br>
+        <a href="Labs/lab3"> Lab3  </a><br>
+        <a href="Labs/lab4/Slider"> Lab4  </a><br>
+        <a href="Labs/lab5/Slider"> Lab5  </a><br>
+        <a href="Homework/HW1"> Homework 1  </a><br>
         <a href="Homework/HW2"> Homework 2  </a><br>
-        
-        <a href="Homework/HW3"> Homework 3  </a>
+        <a href="Homework/HW3"> Homework 3  </a><br>
     </nav>
 </html>
