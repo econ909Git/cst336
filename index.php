b061fed6d59725dabd3745824b455b42c5606164
John Economides <jeconomides@csumb.edu>
2018-11-14 22:38:42 +0000

    link to HW4 correct

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         <a href="Homework/HW1"> Homework 1  </a><br>
         <a href="Homework/HW2"> Homework 2  </a><br>
         <a href="Homework/HW3"> Homework 3  </a><br>
-        <a href="Homework/HW4"> Homework 3  </a><br>
+        <a href="Homework/HW4"> Homework 4  </a><br>
         <a href="sports-project2"> Project 2 </a><br>
     </nav>
 </html>
