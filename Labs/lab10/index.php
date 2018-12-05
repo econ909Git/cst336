5d96ef05bfc517429b48f1fe19c7dc026ed97f14
John Economides <jeconomides@csumb.edu>
2018-12-05 23:50:17 +0000

    added carosel to lab10

--- Labs/lab10/index.php
+++ Labs/lab10/index.php
@@ -18,9 +18,44 @@
         
   <?php 
 	    include 'inc/header.php';
+	    
+	    $images = array("img/alex.jpg", "img/carl.jpg", "img/charlie.jpg", "img/sally.jpg", "img/samantha.jpg", "img/ted.jpg");
+	    
+	    
 	  ?>
         
         <!-- Display Carosel Here -->
+        
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <?php
+                  for ($i=1; $i < 6; $i++) { 
+                    echo "<li data-target='#carouselExampleIndicators' data-slide-to='$i'></li>";
+                  }
+                 ?>
+              </ol>
+              <div class="carousel-inner">
+                <?php
+                  for ($i = 0; $i < 6; $i++) {
+                      echo "<div class=\"carousel-item ";
+                      echo ($i == 0)?" active ":"";
+                      echo "\">";
+                      echo "<img class=\"d-block w-100\" src=\"".$images[$i]."\" alt=\"Second slide\">";
+                      echo "</div>";
+                  }
+                 ?>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+            <br><br>
         
         
         <a class="btn btn-outline-success btn-lg" href="pets.php" role="button">Adopt Now</a>
